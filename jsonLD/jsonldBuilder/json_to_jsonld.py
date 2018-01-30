88c330528ed47bda2bef74eb7516464ee386affd
Ivona Alexandra CHILI <ichili@ias.bitdefender.biz>
2018-01-30 22:33:14 +0200

    Modified JSONldBuilder

--- jsonLD/jsonldBuilder/json_to_jsonld.py
+++ jsonLD/jsonldBuilder/json_to_jsonld.py
@@ -24,7 +24,8 @@ attributes_mapping = {"author_list": ["author", {"@type":"Person"}],
 					  "resource": "contentUrl",
 					  "id_publication": "identifier",
 					  "name_publication": "name",
-					  "publication_list": ["publication", {"@type":"CreativeWork"}]
+					  "publication_list": ["publication", {"@type":"CreativeWork"}],
+					  "learningResourceType": "learningResourceType"
 					  }
 
 annotations_begin = {"@context": "http://schema.org",
@@ -36,56 +37,47 @@ annotations_itemlist = {"@type": "ListItem",
 
 
 
-class JsonLD:
 
-	def __init__(self, json_list):
+def buildJSONLD(content):
+
+	jsonld_results = dict()
+	jsonld_results["results"] = list()
+	for json_to_convert in content["results"]:		
+			
+		json_ld = dict()
+		json_ld["@context"] = annotations_begin["@context"]
+		json_ld["@type"] = annotations_begin["@type"]
+		for key in json_to_convert:
+			values = attributes_mapping[key]
+			if isinstance(values, list):
+				name_key_jsonld = values[0]
+				type_objects = values[1]["@type"]
+				json_ld[name_key_jsonld] = list()
+				list_json = json_to_convert[key]
+				for elem in list_json:
+					d = {}
+					d["@type"] = annotations_itemlist["@type"]
+					d["item"] = dict()
+					d["item"]["@type"] = type_objects
+					for kk in elem:
+						d["item"][attributes_mapping[kk]] = elem[kk]
+					json_ld[name_key_jsonld].append(d)
+			else:
+				json_ld[values] = json_to_convert[key]
+		jsonld_results["results"].append(json_ld)
+	return jsonld_results
 		
-		self.json_list = json_list
-		self.jsonld_list = []
-
-
-	def buildJSONLD(self):
-
-		for j in self.json_list:		
-			
-			json_to_convert = json.loads(j.decode("utf-8"))
-			json_ld = dict()
-			json_ld["@context"] = annotations_begin["@context"]
-			json_ld["@type"] = annotations_begin["@type"]
-			for key in json_to_convert:
-				values = attributes_mapping[key]
-				if isinstance(values, list):
-					name_key_jsonld = values[0]
-					type_objects = values[1]["@type"]
-					json_ld[name_key_jsonld] = list()
-					list_json = json_to_convert[key]
-					for elem in list_json:
-						d = {}
-						d["@type"] = annotations_itemlist["@type"]
-						d["item"] = dict()
-						d["item"]["@type"] = type_objects
-						for kk in elem:
-							d["item"][attributes_mapping[kk]] = elem[kk]
-						json_ld[name_key_jsonld].append(d)
-				else:
-					json_ld[values] = json_to_convert[key]
-			
-			self.jsonld_list.append(json_ld)
-
-	def getJSONLD(self):
-		return self.jsonld_list
 
 
 def main():
-	f = open("json_input", "rb")
-	content = [f.read()]
-	conv = JsonLD(content)
-	conv.buildJSONLD()
-	jsonld_list = conv.getJSONLD()
+	f = open("json_input", "r")
+	content = dict()
+	content["results"] = list()
+	content["results"].append(json.loads(f.read()))		
+	results = buildJSONLD(content)
+
 	g = open("json.out", "w")
-	g.write(json.dumps(jsonld_list[0], indent=4))
-		
-	
+	g.write(json.dumps(results, indent=4))
 
 if __name__ == '__main__':
 	main()
